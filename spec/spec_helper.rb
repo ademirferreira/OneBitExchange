65f7e4060c40fad01ffb1fbf8985bdbb361ed3e3
Ademir Ferreira <ademir.ae86@gmail.com>
2018-06-26 21:32:45 -0300

    add exchange service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
-require File.expand_path("../../config/enviroment", __FILE__)
+require File.expand_path("../../config/environment", __FILE__)
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
