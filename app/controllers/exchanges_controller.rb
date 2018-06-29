3ccfe6b3dc68be383334da59812fce8fc68b2df4
Ademir Ferreira <ademir.ae86@gmail.com>
2018-06-28 21:57:47 -0300

    add excchanges controller

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,5 +3,7 @@ class ExchangesController < ApplicationController
   end
 
   def convert
+    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
+    render json: {"value": value}
   end
 end
